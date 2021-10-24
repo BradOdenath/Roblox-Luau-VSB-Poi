ee88e96fda727c0925810b5399f43af085aa68f7
Brad Odenath <brad.odenath@gmail.com>
2021-10-24 07:01:13 -0400

    Update BlackPoi.lua

--- BlackPoi.lua
+++ BlackPoi.lua
@@ -1,5 +1,5 @@
-Player = game.Players:WaitForChild("bob371")
-Character = game.Workspace:WaitForChild("bob371")
+Player = game.Players:WaitForChild("Visualist")
+Character = game.Workspace:WaitForChild("Visualist")
 script.Parent = Character
 
 wait(2)
@@ -39,17 +39,27 @@ function Part(
 	return p
 end
 
-function Weld(
+function BallSocketConstraint(
 	p0,	--CFrame for Part0
 	p1, --CFrame for Part1
 	cf	--CFrame for C0
 	)
-	local w = Instance.new("Weld")
+	local w = Instance.new("BallSocketConstraint")
+	local a1, a2 = Instance.new("Attachment"), Instance.new("Attachment")
+	a1.Parent = p0
+	a2.Parent = p1
 	w.Parent = p1
+	w.Attachment0 = a1
+	w.Attachment1 = a2
+	a1.CFrame = cf
+	w.LimitsEnabled = true
+	w.TwistLimitsEnabled = true
+	
+	--[[w.Parent = p1
 	w.Part0 = p0
 	w.Part1 = p1
-	w.C0 = cf
-	return w
+	w.C0 = cf]]
+	return a1
 end
 
 --Light Instance (Args: 1)
@@ -122,32 +132,38 @@ function buildPoi(arm)
 	table.insert(_library,_poi)
 	
 	local _knob = Part('Knob', 'Medium stone grey', 'Plastic', 0.17, 0.17, 0.17, _poi) 
-	local _handle = Weld(arm, _knob, CfrAng(0.5,-1,0,0,0,0))
+	local _handle = BallSocketConstraint(arm, _knob, CfrAng(0,-0.2,0,0,0,0))
 	
-	local _ropeA = Part('RopeTop', 'Medium stone grey', 'Plastic', 0.1, 1, 0.1, _poi)
-	local _knobToRopeA = Weld(_knob, _ropeA, CfrAng(0,-0.6,0, 0,0,0))
+	local _ropeA = Part('RopeTop', 'Medium stone grey', 'Plastic', 0.1, 0.5, 0.1, _poi)
+	local _knobToRopeA = BallSocketConstraint(_knob, _ropeA, CfrAng(0,-0.3,0, 0,0,0))
 	local _ropeJ = Part('RopeJoint', 'Black', 'Plastic', 0.12, 0.12, 0.12, _poi) 
-	local _ropeAtoJ = Weld(_ropeA, _ropeJ, CfrAng(0,-0.4,0, 0,0,0))
+	local _ropeAtoJ = BallSocketConstraint(_ropeA, _ropeJ, CfrAng(0,-0.2,0, 0,0,0))
 	
-	local _ropeB = Part('RopeBottom', 'Medium stone grey', 'Plastic', 0.1, 1, 0.1, _poi)
-	local _ropeJtoB = Weld(_ropeJ, _ropeB, CfrAng(0,-0.4,0, 0,0,0))
+	local _ropeB = Part('RopeBottom', 'Medium stone grey', 'Plastic', 0.1, 0.5, 0.1, _poi)
+	local _ropeJtoB = BallSocketConstraint(_ropeJ, _ropeB, CfrAng(0,-0.2,0, 0,0,0))
+	--[[
+	local _ropeJ = Part('RopeJoint', 'Black', 'Plastic', 0.12, 0.12, 0.12, _poi) 
+	local _ropeAtoJ = BallSocketConstraint(_ropeB, _ropeJ, CfrAng(0,-0.2,0, 0,0,0))
 	
+	local _ropeC = Part('RopeCottom', 'Medium stone grey', 'Plastic', 0.1, 0.5, 0.1, _poi)
+	local _ropeJtoC = BallSocketConstraint(_ropeJ, _ropeC, CfrAng(0,-0.2,0, 0,0,0))
+	]]
 	local _head = Part('Head', 'Medium stone grey', 'Plastic', 0.77, 0.77, 0.77, _poi)
 	_head.Shape = "Ball"
-	local _ropeBtoHead = Weld(_ropeB, _head, CfrAng(0,-0.6,0, 0,0,0))
+	local _ropeBtoHead = BallSocketConstraint(_ropeB, _head, CfrAng(0,-0.6,0, 0,0,0))
 	
 	function _library.handleSpin(xyz)
 		if xyz == 'x' then
 			for i = 1, 4 do
-				_handle.C0 = _handle.C0 * Ang(math.pi/32.0,0,0)
+				--_handle.a1 = _handle.a1 * Ang(math.pi/32.0,0,0)
 			end
 		elseif xyz == 'y' then
 			for i = 1, 4 do
-				_handle.C0 = _handle.C0 * Ang(0,math.pi/32.0,0)
+				--_handle.a1 = _handle.a1 * Ang(0,math.pi/32.0,0)
 			end
 		elseif xyz == 'z' then
 			for i = 1, 4 do
-				_handle.C0 = _handle.C0 * Ang(0,0,math.pi/32.0)
+				--_handle.a1 = _handle.a1 * Ang(0,0,math.pi/32.0)
 			end
 		end
 	end
@@ -156,13 +172,13 @@ function buildPoi(arm)
 end
 
 function manipulateCharacter()
-	local leftPoi = buildPoi(Character['LeftLowerArm'])
-	local rightPoi = buildPoi(Character['RightLowerArm'])
+	local leftPoi = buildPoi(Character['LeftHand'])
+	local rightPoi = buildPoi(Character['RightHand'])
 	leftPoi.handleSpin('x')
-	while true do wait()
+	--[[while true do wait()
 		leftPoi.handleSpin('x')
 		rightPoi.handleSpin('x')
-	end
+	end]]
 end
 
 manipulateCharacter()
